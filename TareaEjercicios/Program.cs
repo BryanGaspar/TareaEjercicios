1efb35593fb13b9e3438f0357695dccc98d50a38
BRYAN GASPAR <bryan.gaspar@pucese.edu.ec>
2020-11-14 23:20:00 -0500

    Tarea Ejercicos en clase y ejercicios condicional-if

---
+++ TareaEjercicios/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TareaEjercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void figura()
+            {
+                Console.WriteLine("    *    ");
+                Console.WriteLine("   *   ");
+                Console.WriteLine("  ***  ");
+                Console.WriteLine(" ***** ");
+            }
+
+            //Crear la función que devuelva un parámetro entero y devuelva el cuadrado de ese entero
+            static int cuadrado(int a)
+            {
+                int cuadrado = a * a;
+                return cuadrado;
+            }
+        }
+    }
+}
